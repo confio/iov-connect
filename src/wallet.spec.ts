ad918b7b3e018984d983ace6d1bb3cb23d579bce
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-07-04 00:36:46 +0200

    Helpers to create new account with funds

--- src/wallet.spec.ts
+++ src/wallet.spec.ts
@@ -1,14 +1,28 @@
-import { createWallet, walletAddress } from "./wallet";
+import { connect, createWallet, disconnect, ensureWalletFunds, pprint } from "./wallet";
 
 describe("createWallet", () => {
-    it("returns a deterministic address", async () => {
-        const wallet = await createWallet();
-        const addr = walletAddress(wallet);
-        console.log(addr);
+  it("returns a deterministic address", async () => {
+    const wallet = await createWallet();
+    expect(wallet.address).toBeTruthy();
 
-        const wallet2 = await createWallet(wallet.mnemonic);
-        const addr2 = walletAddress(wallet2);
+    const wallet2 = await createWallet(wallet.mnemonic);
 
-        expect(addr).toEqual(addr2);
-    })
+    expect(wallet2.address).toEqual(wallet.address);
+  });
+});
+
+describe("ensureAccount", () => {
+  it("creates a new account", async () => {
+    const wallet = await createWallet();
+    const conn = await connect(wallet);
+    try {
+      const acct = await ensureWalletFunds(conn);
+      expect(acct).toBeDefined();
+      pprint(acct.address);
+      pprint(acct.balance);
+      expect(acct.balance.length).toEqual(1);
+    } finally {
+      disconnect(conn);
+    }
+  });
 });
--- src/wallet.ts
+++ src/wallet.ts
@@ -1,21 +1,30 @@
-import { Address, ChainId, Identity } from "@iov/bcp";
-import { bnsCodec } from "@iov/bns";
+import { Account, Address, BlockchainConnection, ChainId, Identity, TokenTicker } from "@iov/bcp";
+import { bnsCodec, bnsConnector, BnsConnection } from "@iov/bns";
+import { MultiChainSigner } from '@iov/core';
 import { Bip39, Random } from "@iov/crypto";
-import { Encoding } from "@iov/encoding";
+import { IovFaucet } from "@iov/faucets";
 import { Ed25519HdWallet, HdPaths, UserProfile } from "@iov/keycontrol";
 
+// import { Encoding } from "@iov/encoding";
 // const { fromHex, toHex } = Encoding;
 
 export const antnet = {
-  id: "bov-antnet" as ChainId,
-  url: "https://bns.antnet.iov.one:443",
+  id: "bns-antnet" as ChainId,
+  url: "wss://bns.antnet.iov.one:443",
   faucet: "https://bns-faucet.antnet.iov.one",
+  token: "IOV" as TokenTicker,
 };
 
 export interface Wallet {
   readonly profile: UserProfile;
   readonly id: Identity;
   readonly mnemonic: string;
+  readonly address: Address;
+}
+
+export interface Connection extends Wallet {
+  readonly signer: MultiChainSigner;
+  readonly query: BlockchainConnection;
 }
 
 // create wallet will initialize a wallet with a random new, or a provided, mnemonic
@@ -36,9 +45,35 @@ export async function createWallet(fromMnemonic?: string): Promise<Wallet> {
   const id = await profile.createIdentity(walletId, antnet.id, HdPaths.iov(0));
   profile.setIdentityLabel(id, "main account");
 
-  return { profile, id, mnemonic };
+  const address = bnsCodec.identityToAddress(id);
+
+  return { profile, id, mnemonic, address };
 }
 
-export function walletAddress(wallet: Wallet): Address {
-  return bnsCodec.identityToAddress(wallet.id);
+export async function connect(wallet: Wallet): Promise<Connection> {
+  const signer = new MultiChainSigner(wallet.profile);
+  await signer.addChain(bnsConnector(antnet.url, antnet.id));
+  const query = signer.connection(antnet.id);
+  return {...wallet, signer, query};
+}
+
+export async function ensureWalletFunds(conn: Connection): Promise<Account> {
+  let acct = await conn.query.getAccount({ address: conn.address });
+  if (acct === undefined) {
+    const faucet = new IovFaucet(antnet.faucet);
+    await faucet.credit(conn.address, antnet.token);
+    acct = await conn.query.getAccount({ address: conn.address });
+    if (acct === undefined) {
+      throw new Error("Account still undefined after faucet");
+    }
+  }
+  return acct;
+}
+
+export function disconnect(conn: Connection): void {
+  conn.signer.shutdown();
+}
+
+export function pprint(obj: any): void {
+  console.log(JSON.stringify(obj, null, 2));
 }
