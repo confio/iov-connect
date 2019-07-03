436d1a31742afb38d6f441acaa1575e1960962cc
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-07-04 00:54:41 +0200

    Test send tx

--- src/wallet.spec.ts
+++ src/wallet.spec.ts
@@ -1,4 +1,5 @@
-import { connect, createWallet, disconnect, ensureWalletFunds, pprint } from "./wallet";
+import { SendTransaction, WithCreator} from "@iov/bcp"
+import { antnet, connect, createWallet, disconnect, ensureWalletFunds, pprint } from "./wallet";
 
 describe("createWallet", () => {
   it("returns a deterministic address", async () => {
@@ -21,6 +22,39 @@ describe("ensureAccount", () => {
       pprint(acct.address);
       pprint(acct.balance);
       expect(acct.balance.length).toEqual(1);
+    } finally {
+      disconnect(conn);
+    }
+  });
+
+  it("can send tokens", async () => {
+    const mine = await createWallet();
+    const other = await createWallet();
+    const conn = await connect(mine);
+    try {
+      const acct = await ensureWalletFunds(conn);
+      let yours = await conn.query.getAccount({address: other.address});
+      expect(yours).toBeUndefined();
+
+      const sendTx: SendTransaction & WithCreator = {
+        kind: "bcp/send",
+        creator: mine.id,
+        sender: mine.address, // this account must have money
+        recipient: other.address,
+        memo: "My first transaction",
+        amount: { // 0.123 IOV (9 sig figs in tx codec)
+          quantity: '123000000',
+          fractionalDigits: 9,
+          tokenTicker: antnet.token,
+        },
+      };
+
+      await conn.signer.signAndPost(sendTx);
+
+      yours = await conn.query.getAccount({address: other.address});
+      expect(yours).toBeDefined();
+      expect(yours!.balance.length).toEqual(1);
+      expect(yours!.balance[0].tokenTicker).toEqual(antnet.token);
     } finally {
       disconnect(conn);
     }
